90e44dab387b99377e26e0914d97f6976f85bd57
Bastian Bernal <babv20@gmail.com>
2025-08-28 13:58:39 -0400

    GREEN: se modifican funciones para que funcionen en rondas especiales

--- src/juego/arbitro_ronda.py
+++ src/juego/arbitro_ronda.py
@@ -3,37 +3,45 @@ class ArbitroRonda:
         pass
 
     @staticmethod
-    def _total_pinta(estado_cachos, pinta):
+    def _total_pinta(estado_cachos, pinta, obligar = False):
         """
         Retorna la cantidad total de cachos que hay de una pinta determinada,
         incluyendo los cachos comodín (pinta 1).
         :param estado_cachos: Diccionario con la cantidad de dados con cierta pinta.
         :param pinta: La pinta a verificar.
+        :param obligar: Indica si existe un jugador con un solo dado.
         :return: Cantidad total de cachos de la pinta especificada.
         """
-        return estado_cachos.get(pinta, 0) + estado_cachos.get(1, 0)
+        if not obligar:
+            if pinta != 1:
+                return estado_cachos.get(pinta, 0) + estado_cachos.get(1, 0)
+            return estado_cachos.get(1, 0)
+        return estado_cachos.get(pinta, 0)
 
-    def resultado_duda(self, estado_cachos, pinta, cantidad):
+
+    def resultado_duda(self, estado_cachos, pinta, cantidad, obligar = False):
         """
         Retorna si la duda fue correcta (si la cantidad de dados de la pinta
         especificada es menor a la cantidad apostada).
         :param estado_cachos: Diccionario con la cantidad de dados con cierta pinta.
         :param pinta: La pinta a verificar.
         :param cantidad: La cantidad apostada.
+        :param obligar: Indica si existe un jugador con un solo dado.
         :return: True si la duda fue correcta, False en caso contrario.
         """
-        return self._total_pinta(estado_cachos, pinta) < cantidad
+        return self._total_pinta(estado_cachos, pinta, obligar) < cantidad
 
-    def resultado_calzo(self, estado_cachos, pinta, cantidad):
+    def resultado_calzo(self, estado_cachos, pinta, cantidad, obligar = False):
         """
         Retorna si el calzo fue correcto (si la cantidad de dados de la pinta
         especificada es igual a la cantidad apostada).
         :param estado_cachos: Diccionario con la cantidad de dados con cierta pinta.
         :param pinta: La pinta a verificar.
         :param cantidad: La cantidad apostada.
+        :param obligar: Indica si existe un jugador con un solo dado.
         :return: True si el calzo fue correcto, False en caso contrario.
         """
-        return self._total_pinta(estado_cachos, pinta) == cantidad
+        return self._total_pinta(estado_cachos, pinta, obligar) == cantidad
 
     @staticmethod
     def validar_calzo(estado_cachos, cantidad_cachos, existe_jugador_con_un_dado=False):
