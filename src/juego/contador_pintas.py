04587de9dcc4983a4247fc045b9ab0f709cd08d5
Emyowo204 <egaete2023@inf.udec.cl>
2025-08-29 22:50:12 -0400

    GREEN: implementación mínima para contar la cantidad de pintas totales en todo el juego

--- src/juego/contador_pintas.py
+++ src/juego/contador_pintas.py
@@ -15,3 +15,15 @@ class Contador_Pintas:
             if valor == pinta:
                 cantidad += 1
         return cantidad
+
+    def contar_pintas_totales(self, lista_cachos, pinta):
+        cantidad = 0
+        for cacho in lista_cachos:
+            cantidad += self.contar_pintas(cacho, pinta)
+        return cantidad
+
+    def contar_pintas_totales_sinAs(self, lista_cachos, pinta):
+        cantidad = 0
+        for cacho in lista_cachos:
+            cantidad += self.contar_pintas_sinAs(cacho, pinta)
+        return cantidad
