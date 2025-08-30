68cbbc8f8b2eb5a6313ef5f128fe9b2ff79270c4
Emyowo204 <egaete2023@inf.udec.cl>
2025-08-29 22:43:52 -0400

    REFACTOR: mejora código para contar las pintas de los dados dentro de un cacho

--- src/juego/contador_pintas.py
+++ src/juego/contador_pintas.py
@@ -1,15 +1,17 @@
+from src.juego.cacho import Cacho
+
 class Contador_Pintas:
 
-    def contar_pintas(self, lista, pinta):
+    def contar_pintas(self, cacho, pinta):
         cantidad = 0
-        for valor in lista:
+        for valor in cacho.mirar():
             if valor == pinta or valor == 'As':
                 cantidad += 1
         return cantidad
 
-    def contar_pintas_sinAs(self, lista, pinta):
+    def contar_pintas_sinAs(self, cacho, pinta):
         cantidad = 0
-        for valor in lista:
+        for valor in cacho.mirar():
             if valor == pinta:
                 cantidad += 1
         return cantidad
--- tests/test_contador_pintas.py
+++ tests/test_contador_pintas.py
@@ -4,19 +4,16 @@ from src.juego.contador_pintas import Contador_Pintas
 def test_contar_pintas_del_cacho():
     valoresIniciales = ['As', 'Quina', 'As', 'Tonto', 'Tren']
     cacho = Cacho(valoresIniciales)
-    valores = cacho.mirar()
     contador = Contador_Pintas()
 
-    total = contador.contar_pintas(valores, 'Quina')
+    total = contador.contar_pintas(cacho, 'Quina')
     # La cantidad de dados con la pinta especificada es correcta
     assert total == 3
 
-    total = contador.contar_pintas(valores, 'As')
+    total = contador.contar_pintas(cacho, 'As')
     # La cantidad de dados con la pinta As es correcta
     assert total == 2
 
-    total = contador.contar_pintas_sinAs(valores, 'Quina')
+    total = contador.contar_pintas_sinAs(cacho, 'Quina')
     # La cantidad de dados con la pinta especificada, excluyendo al As como comodín es correcta
     assert total == 1
-
-
