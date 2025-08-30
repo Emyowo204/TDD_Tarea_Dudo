1ab19127c3723b6b7e009e5db15dbba67651448c
Emyowo204 <egaete2023@inf.udec.cl>
2025-08-29 22:29:09 -0400

    RED: test para contar las pintas de los dados dentro de un cacho - falla como esperado

---
+++ tests/test_contador_pintas.py
@@ -0,0 +1,22 @@
+from src.juego.cacho import Cacho
+from src.juego.contador_pintas import Contador_Pintas
+
+def test_contar_pintas_del_cacho():
+    valoresIniciales = ['As', 'Quina', 'As', 'Tonto', 'Tren']
+    cacho = Cacho(valoresIniciales)
+    valores = cacho.mirar
+    contador = Contador_Pintas()
+
+    total = contador.contar_pintas(valores, 'Quina')
+    # La cantidad de dados con la pinta especificada es correcta
+    assert total == 3
+
+    total = contador.contar_pintas(valores, 'As')
+    # La cantidad de dados con la pinta As es correcta
+    assert total == 2
+
+    total = contador.contar_pintas_sinAs(valores, 'Quina')
+    # La cantidad de dados con la pinta especificada, excluyendo al As como comodín es correcta
+    assert total == 1
+
+
