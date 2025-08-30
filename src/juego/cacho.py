76f26a55caa6ebea6f7d9966e67620623d9e29a5
Emyowo204 <egaete2023@inf.udec.cl>
2025-08-29 21:58:32 -0400

    GREEN: implementación mínima para agitar un cacho

--- src/juego/cacho.py
+++ src/juego/cacho.py
@@ -1,12 +1,21 @@
+from src.juego.dado import Dado
+
 class Cacho:
     __valores = None
     __cantidad = None
+    __dado = None
 
     def __init__(self, iniciales=None):
         self.__valores = iniciales
         self.__cantidad = 5
+        self.__dado = Dado()
         if iniciales is not None:
             self.__cantidad = len(iniciales)
 
     def mirar(self):
         return self.__valores
+
+    def agitar(self):
+        self.__valores = []
+        for i in range(self.__cantidad):
+            self.__valores.append(self.__dado.lanzar())
