2d8d4271ff038f14ad5db9cb17c9ff78438f49d2
Emyowo204 <egaete2023@inf.udec.cl>
2025-08-29 21:52:05 -0400

    RED: test para agitar un cacho - falla como esperado

--- tests/test_cacho.py
+++ tests/test_cacho.py
@@ -5,3 +5,11 @@ def test_mirar_cacho():
     cacho = Cacho(valoresEsperado)
     valores = cacho.mirar()
     assert valores == valoresEsperado
+
+def test_agitar_cacho():
+    cacho = Cacho()
+    cacho.agitar()
+    valores = cacho.mirar()
+
+    assert set(valores).issubset({'As', 'Tonto', 'Tren', 'Cuadra', 'Quina', 'Sexto'})
+    assert len(valores) == 5
