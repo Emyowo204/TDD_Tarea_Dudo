9c641761bc09e648d5beb729118059a9d3b1839a
Emyowo204 <egaete2023@inf.udec.cl>
2025-08-29 22:47:50 -0400

    RED: test para contar la cantidad de pintas totales en todo el juego - falla como esperado

--- tests/test_contador_pintas.py
+++ tests/test_contador_pintas.py
@@ -17,3 +17,23 @@ def test_contar_pintas_del_cacho():
     total = contador.contar_pintas_sinAs(cacho, 'Quina')
     # La cantidad de dados con la pinta especificada, excluyendo al As como comodín es correcta
     assert total == 1
+
+def test_contar_pintas_totales():
+    cacho1 = Cacho(['As', 'Quina', 'As', 'Tonto', 'Tren'])
+    cacho2 = Cacho(['Sexto', 'As', 'Tren', 'Tonto', 'Cuadra'])
+    cacho3 = Cacho(['Quina', 'Sexto', 'Cuadra', 'As', 'Quina'])
+
+    cachosEnJuego = [cacho1, cacho2, cacho3]
+    contador = Contador_Pintas()
+
+    total = contador.contar_pintas_totales(cachosEnJuego, 'Quina')
+    # La cantidad de dados con la pinta especificada en el juego es correcta
+    assert total == 7
+
+    total = contador.contar_pintas_totales(cachosEnJuego, 'As')
+    # La cantidad de dados con la pinta As en el juego es correcta
+    assert total == 4
+
+    total = contador.contar_pintas_totales_sinAs(cachosEnJuego, 'Quina')
+    # La cantidad de dados con la pinta especificada en el juego, excluyendo al As como comodín es correcta
+    assert total == 3
