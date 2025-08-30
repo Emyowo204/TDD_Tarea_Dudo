47b0b672179ba58516a2e2535499ab4b88e3cb07
Emyowo204 <egaete2023@inf.udec.cl>
2025-08-29 21:45:07 -0400

    REFACTOR: mejora código para mirar los dados de un cacho

--- src/juego/cacho.py
+++ src/juego/cacho.py
@@ -1,8 +1,12 @@
 class Cacho:
     __valores = None
+    __cantidad = None
 
     def __init__(self, iniciales=None):
         self.__valores = iniciales
+        self.__cantidad = 5
+        if iniciales is not None:
+            self.__cantidad = len(iniciales)
 
     def mirar(self):
         return self.__valores
