b522ca1e8c32b2c8d3456833bfaf2d0a9800f521
Emyowo204 <egaete2023@inf.udec.cl>
2025-08-29 22:05:48 -0400

    RED: test para quitar un dado de un cacho - falla como esperado

--- tests/test_cacho.py
+++ tests/test_cacho.py
@@ -13,3 +13,16 @@ def test_agitar_cacho():
 
     assert set(valores).issubset({'As', 'Tonto', 'Tren', 'Cuadra', 'Quina', 'Sexto'})
     assert len(valores) == 5
+
+def test_quitar_dado():
+    valoresTest = ['As', 'Quina', 'As']
+    cacho = Cacho(valoresTest)
+    cacho.quitar_dado()
+
+    assert len(cacho.mirar) == (len(valoresTest)-1)
+
+    valoresTest = []
+    cacho = Cacho(valoresTest)
+    cacho.quitar_dado()
+
+    assert len(cacho.mirar) == 0
