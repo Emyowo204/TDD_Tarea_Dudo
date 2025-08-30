8789659a781a2b4422d736a87b7485d6824ccce4
Emyowo204 <egaete2023@inf.udec.cl>
2025-08-29 22:14:01 -0400

    REFACTOR: mejora código de mirar y agitar cachos

--- tests/test_cacho.py
+++ tests/test_cacho.py
@@ -4,14 +4,17 @@ def test_mirar_cacho():
     valoresEsperado = ['As', 'Quina', 'As']
     cacho = Cacho(valoresEsperado)
     valores = cacho.mirar()
+    # Los valores de los dados son los esperados
     assert valores == valoresEsperado
 
 def test_agitar_cacho():
     cacho = Cacho()
     cacho.agitar()
     valores = cacho.mirar()
-
+    # Los valores de los dados están dentro de la lista de pintas
     assert set(valores).issubset({'As', 'Tonto', 'Tren', 'Cuadra', 'Quina', 'Sexto'})
+
+    # La cantidad de dados es la correcta
     assert len(valores) == 5
 
 def test_quitar_dado():
@@ -19,11 +22,11 @@ def test_quitar_dado():
     cacho = Cacho(valoresTest)
     cacho.quitar_dado()
     valores = cacho.mirar()
-
+    # La cantidad de dados se resta correctamente
     assert len(valores) == (len(valoresTest)-1)
 
     cacho = Cacho([])
     cacho.quitar_dado()
     valores = cacho.mirar()
-
+    # Evitar tener un número negativo de dados
     assert len(valores) == 0
