ed20a1cfe7ed44501d6cd6abc3aaee6d2726d406
Emyowo204 <egaete2023@inf.udec.cl>
2025-09-01 22:09:41 -0400

    REFACTOR: mejora código de la validación de una nueva apuesta con respecto a la apuesta anterior

--- src/juego/validador_apuesta.py
+++ src/juego/validador_apuesta.py
@@ -1,20 +1,32 @@
 import math
 
+""" Valida si una nueva apuesta con respecto a las condiciones de la apuesta anterior. """
 class Validador_Apuesta:
 
+    """ Lista con los nombres de las pintas del dado, de 1 a 6. """
     __pintas = ['As', 'Tonto', 'Tren', 'Cuadra', 'Quina', 'Sexto']
 
     def __convertir(self, pinta):
+        """
+        Convierte una pinta del juego Dudo en su valor numérico.
+        :param pinta: La pinta a convertir.
+        :return: El valor numérico de la pinta convertida (Del 1 al 6).
+        """
         numero = 0
         for i in range(len(self.__pintas)):
             if self.__pintas[i] == pinta:
                 numero = i+1
         return numero
 
-    def __esPar(self, numero):
-        return numero % 2 == 0
-
     def validar(self, numIni, pintaIni, numNuevo, pintaNueva):
+        """
+        Validación de la apuesta realizada, según la apuesta anterior.
+        :param numIni: La cantidad de la apuesta anterior.
+        :param pintaIni: La pinta de la apuesta anterior.
+        :param numNuevo: La cantidad de la apuesta realizada actualmente.
+        :param pintaNueva: La pinta de la apuesta realizada actualmente.
+        :return: Booleano con la validación de la apuesta (Valida / No valida).
+        """
         if pintaIni == None and (pintaNueva == 'As' or numNuevo <=0):
             return False
 
@@ -29,9 +41,9 @@ class Validador_Apuesta:
             return False
 
         elif pintaNueva == 'As':
-            if self.__esPar(numIni) and numNuevo >= (numIni / 2) + 1:
+            if numIni % 2 == 0 and numNuevo >= (numIni / 2) + 1:
                 return True
-            if not self.__esPar(numIni) and numNuevo >= math.ceil(numIni / 2):
+            if numIni % 2 != 0 and numNuevo >= math.ceil(numIni / 2):
                 return True
 
         else:
